e1d20e5021309dbd8835168516abf4cff945e758
tikwid <arrron@gmail.com>
2014-02-10 21:09:18 +1300

    Update index.js

--- arron/index.js
+++ arron/index.js
@@ -1,1 +1,6 @@
 //js
+
+$("#csnotes_click").click(function () {
+  $("#title").empty().load("csnotes.html #title").fadeIn("slow");
+  $("#content").empty().load("csnotes.html #content").fadeIn("slow");
+  });
